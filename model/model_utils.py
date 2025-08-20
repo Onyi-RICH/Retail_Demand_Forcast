21af6df59c165dd085eb48b5284bc407cbcd06f5
OnyiUgba <onyi.ugba@gmail.com>
2025-08-20 07:02:37 +0200

    Update model_utils.py

--- model/model_utils.py
+++ model/model_utils.py
@@ -6,7 +6,3 @@ from app import config
 def load_model(model_path: str = config.MODEL_PATH):
     """Load trained model from .pkl file."""
     return joblib.load(model_path)
-
-def forecast(model, input_data: pd.DataFrame):
-    """Generate single-step predictions from model."""
-    return model.predict(input_data)
